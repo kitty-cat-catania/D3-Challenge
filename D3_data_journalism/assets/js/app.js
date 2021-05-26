f20c0e9fc78e41efe54248a7197ad5306469ff37
toothFairyDragon <gabrielle.catania@gmail.com>
2021-05-26 12:58:15 -0700

    Appended a test circle to the div where the scatter plot should go

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,66 @@
 // @TODO: YOUR CODE HERE!
+d3.csv("./assets/data/data.csv").then(function(data) {
+    console.log(data);
+
+    /*define svg area dimensions
+    var svgWidth = 1000;
+    var svgHeight = 600;
+
+    var margin = {
+        top: 20,
+        right: 40,
+        bottom: 80,
+        left:50
+    };
+
+    var width = svgWidth-margin.left -margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
+
+    var svg = d3
+        .select("scatter")
+        .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+
+
+    //append svg group
+    var chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    var chosenXAxis = "poverty";
+
+    var xLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.poverty)])
+        .range([0,width]);
+    
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.obesity)])
+        .range([0, height]);
+
+    //create axis funcitons
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    //add x-axis 
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    */
+
+    //try appending an svg 
+    var svg = d3.select("#scatter").append("svg");
+    svg.attr("width", "50px").attr("height", "50px");
+    
+    
+    svg.append("circle")
+        .attr("cx", 10)
+        .attr("cy", 25)
+        .attr("r", 3)
+        .attr("stroke", "black")
+        .attr("stroke-width", "5")
+        .attr("fill", "purple");
+
+
+
+});
